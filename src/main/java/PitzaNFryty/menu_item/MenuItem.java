b59491875c3082e9f9f9386a4faa9bccf61b6f0f
Michał Puła <michal.pula98@gmail.com>
2021-02-10 12:13:28 +0100

    feat: add MenuItem abstract class

---
+++ src/main/java/PitzaNFryty/menu_item/MenuItem.java
@@ -0,0 +1,58 @@
+package PitzaNFryty.menu_item;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "menu_items")
+public abstract class MenuItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, updatable = false)
+    private long id;
+
+    @Column(name = "name", unique = true)
+    private String name;
+
+    @Column(name = "available")
+    private boolean available;
+
+    @Column(name = "image_url", unique = true)
+    private String imageURL ;
+
+    public MenuItem() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+}
