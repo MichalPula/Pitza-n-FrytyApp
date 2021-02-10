29ab052569d519f6dc34f2eac0ed8aca3849dd7e
Michał Puła <michal.pula98@gmail.com>
2021-02-10 12:45:18 +0100

    fix: add @Repository annotation to all repos

--- src/main/java/PitzaNFryty/menu_item/fries/FriesRepository.java
+++ src/main/java/PitzaNFryty/menu_item/fries/FriesRepository.java
@@ -1,6 +1,8 @@
 package PitzaNFryty.menu_item.fries;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FriesRepository extends JpaRepository<Fries, Long> {
 }
