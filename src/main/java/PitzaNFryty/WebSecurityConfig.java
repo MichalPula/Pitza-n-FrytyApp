4ab279463144c73fc23ecb4b67088bd7cd05ed89
Michał Puła <michal.pula98@gmail.com>
2021-02-11 12:09:12 +0100

    feat: add WebSecurityConfig and disable Cross Site Request Forgery

--- src/main/java/PitzaNFryty/WebSecurityConfig.java
+++ src/main/java/PitzaNFryty/WebSecurityConfig.java
@@ -1,10 +1,16 @@
-package PitzaNFryty.menu_item;
+package PitzaNFryty;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
 }
