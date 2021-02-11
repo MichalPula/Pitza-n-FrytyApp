afb32577eb3b984832bf1e690d6ce376cc1d3dd9
Michał Puła <michal.pula98@gmail.com>
2021-02-11 12:02:55 +0100

    feat: add getAll() method impl in FriesController

--- src/main/java/PitzaNFryty/menu_item/fries/FriesController.java
+++ src/main/java/PitzaNFryty/menu_item/fries/FriesController.java
@@ -1,10 +1,28 @@
 package PitzaNFryty.menu_item.fries;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("fries")
 public class FriesController {
+
+    private final FriesService friesService;
+
+    @Autowired
+    public FriesController(FriesService friesService) {
+        this.friesService = friesService;
+    }
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Fries>> getAll(){
+        return ResponseEntity.ok().body(this.friesService.getAll());
+    }
 
 }
