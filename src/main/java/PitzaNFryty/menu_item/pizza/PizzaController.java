1cda4264cc288d66c1e74ee41ed5ae31eb4ec42f
Michał Puła <michal.pula98@gmail.com>
2021-02-21 20:57:47 +0100

    feat: add PizzaController with getAll method

---
+++ src/main/java/PitzaNFryty/menu_item/pizza/PizzaController.java
@@ -0,0 +1,27 @@
+package PitzaNFryty.menu_item.pizza;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("pizza")
+public class PizzaController {
+
+    private final PizzaService pizzaService;
+
+    @Autowired
+    public PizzaController(PizzaService pizzaService) {
+        this.pizzaService = pizzaService;
+    }
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Pizza>> getAll() {
+        return ResponseEntity.ok().body(pizzaService.getAll());
+    }
+}
