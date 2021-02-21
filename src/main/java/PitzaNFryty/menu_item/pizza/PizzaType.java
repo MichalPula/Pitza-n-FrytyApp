61a14cae3fa68379caa1a56b69d1c8ea22be1965
Michał Puła <michal.pula98@gmail.com>
2021-02-21 19:06:49 +0100

    feat: add PizzaType class

--- src/main/java/PitzaNFryty/menu_item/pizza/PizzaType.java
+++ src/main/java/PitzaNFryty/menu_item/pizza/PizzaType.java
@@ -1,4 +1,47 @@
 package PitzaNFryty.menu_item.pizza;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "pizzas_sizes")
 public class PizzaType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "pizza_size", unique = true)
+    private PizzaSizePrice sizePrice;
+
+    public PizzaType(PizzaSizePrice sizePrice) {
+        this.sizePrice = sizePrice;
+    }
+
+    public PizzaType() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public PizzaSizePrice getSizePrice() {
+        return sizePrice;
+    }
+
+    public void setSizePrice(PizzaSizePrice sizePrice) {
+        this.sizePrice = sizePrice;
+    }
+
+    @Override
+    public String toString() {
+        return this.getSizePrice().name() + "-" +
+                this.getSizePrice().getPrice() + "-" +
+                this.getSizePrice().getDiameter()+"cm";
+    }
 }
