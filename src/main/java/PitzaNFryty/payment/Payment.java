6a798c74c0321755a4efbf2fdfe2fda3d715bded
Michał Puła <michal.pula98@gmail.com>
2021-03-01 12:12:09 +0100

    feat: fill Payment class

--- src/main/java/PitzaNFryty/payment/Payment.java
+++ src/main/java/PitzaNFryty/payment/Payment.java
@@ -1,6 +1,11 @@
 package PitzaNFryty.payment;
 
+import PitzaNFryty.customer.Customer;
+import com.sun.istack.NotNull;
+import org.joda.money.Money;
+
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "payments")
@@ -9,4 +14,56 @@ public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+    @ManyToOne(targetEntity = Customer.class)
+    private Customer customer;
+
+    @NotNull
+    @Column(name = "money")
+    private Money money;
+
+    @NotNull
+    private LocalDateTime time;
+
+    public Payment(Customer customer, Money money, LocalDateTime time) {
+        this.customer = customer;
+        this.money = money;
+        this.time = time;
+    }
+
+    public Payment() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Money getMoney() {
+        return money;
+    }
+
+    public void setMoney(Money money) {
+        this.money = money;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
 }
