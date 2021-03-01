55f99f0a30e972248a76cd8b3c1491e65ab8d9cf
Michał Puła <michal.pula98@gmail.com>
2021-03-01 11:04:11 +0100

    feat: add Address class

---
+++ src/main/java/PitzaNFryty/address/Address.java
@@ -0,0 +1,92 @@
+package PitzaNFryty.address;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "addresses")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    private String city;
+
+    @NotNull
+    @Column(name = "zip_code")
+    private String zipCode;
+
+    @NotNull
+    private String street;
+
+    @NotNull
+    @Column(name = "building_number")
+    private String buildingNumber;
+
+    @NotNull
+    @Column(name = "flat_number")
+    private String flatNumber;
+
+    public Address(String city, String zipCode, String street, String buildingNumber, String flatNumber) {
+        this.city = city;
+        this.zipCode = zipCode;
+        this.street = street;
+        this.buildingNumber = buildingNumber;
+        this.flatNumber = flatNumber;
+    }
+
+    public Address() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getBuildingNumber() {
+        return buildingNumber;
+    }
+
+    public void setBuildingNumber(String buildingNumber) {
+        this.buildingNumber = buildingNumber;
+    }
+
+    public String getFlatNumber() {
+        return flatNumber;
+    }
+
+    public void setFlatNumber(String flatNumber) {
+        this.flatNumber = flatNumber;
+    }
+}
