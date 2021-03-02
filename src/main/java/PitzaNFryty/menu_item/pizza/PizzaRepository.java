75d08c27454a731fb57941d3d03580089522b17a
Michał Puła <michal.pula98@gmail.com>
2021-03-02 11:12:36 +0100

    feat: add findByName method

--- src/main/java/PitzaNFryty/menu_item/pizza/PizzaRepository.java
+++ src/main/java/PitzaNFryty/menu_item/pizza/PizzaRepository.java
@@ -3,4 +3,5 @@ package PitzaNFryty.menu_item.pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Long> {
+    Pizza findByName(String name);
 }
