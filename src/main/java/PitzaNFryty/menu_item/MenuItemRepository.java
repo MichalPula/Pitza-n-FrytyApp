5f86002ff339c54eabdfd1deefe7ce25be17532c
Michał Puła <michal.pula98@gmail.com>
2021-03-07 16:08:39 +0100

    fix: add @Repository annotation to repos

--- src/main/java/PitzaNFryty/menu_item/MenuItemRepository.java
+++ src/main/java/PitzaNFryty/menu_item/MenuItemRepository.java
@@ -1,9 +1,11 @@
 package PitzaNFryty.menu_item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
     MenuItem findByNameContainingIgnoreCase(String name);
     List<MenuItem> findAllByNameInIgnoreCase(List<String> menuItems);
