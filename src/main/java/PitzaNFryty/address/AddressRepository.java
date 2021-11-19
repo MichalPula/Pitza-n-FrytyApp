a0269aff7cc138652f588b48e87d83ef02ca22b2
Michał Puła <michal.pula98@gmail.com>
2021-11-19 14:45:45 +0100

    feat: add getCustomerAddresses method with custom JPQL @Query

--- src/main/java/PitzaNFryty/address/AddressRepository.java
+++ src/main/java/PitzaNFryty/address/AddressRepository.java
@@ -1,8 +1,15 @@
 package PitzaNFryty.address;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+
+    @Query("select new PitzaNFryty.address.AddressReadDTO(a.id, a.city, a.zipCode, a.street, a.buildingNumber, a.flatNumber)" +
+            " from Address a where a.customer.id = :customerId")
+    List<AddressReadDTO> getCustomerAddresses(Long customerId);
 }
