c4bbe787d7d9df8443ca67e857c9b455e59e6759
Michał Puła <michal.pula98@gmail.com>
2021-11-19 13:11:40 +0100

    refactor: make both classes use Lombok, change GenerationType to SEQUENCE and delete @LazyToOne

--- src/main/java/PitzaNFryty/address/Address.java
+++ src/main/java/PitzaNFryty/address/Address.java
@@ -1,25 +1,28 @@
 package PitzaNFryty.address;
 
-
 import PitzaNFryty.customer.Customer;
 import com.sun.istack.NotNull;
-import org.hibernate.annotations.LazyToOne;
-import org.hibernate.annotations.LazyToOneOption;
+import lombok.*;
 
 import javax.persistence.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 @Entity
 @Table(name = "addresses")
 public class Address {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(name = "address_id_generator", sequenceName = "address_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "address_id_generator")
     @Column(name = "id", unique = true, updatable = false)
     private long id;
 
-    @ManyToOne(targetEntity = Customer.class)
-    @LazyToOne(LazyToOneOption.NO_PROXY)
-    @JoinColumn(name = "customer_id")
+    @ManyToOne(targetEntity = Customer.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id", referencedColumnName = "id")
+    @ToString.Exclude
     private Customer customer;
 
     @NotNull
@@ -49,85 +52,5 @@ public class Address {
         this.street = street;
         this.buildingNumber = buildingNumber;
         this.flatNumber = flatNumber;
-    }
-
-    public Address(String city, String zipCode, String street, String buildingNumber, String flatNumber) {
-        this.city = city;
-        this.zipCode = zipCode;
-        this.street = street;
-        this.buildingNumber = buildingNumber;
-        this.flatNumber = flatNumber;
-    }
-
-    public Address() {
-
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getBuildingNumber() {
-        return buildingNumber;
-    }
-
-    public void setBuildingNumber(String buildingNumber) {
-        this.buildingNumber = buildingNumber;
-    }
-
-    public String getFlatNumber() {
-        return flatNumber;
-    }
-
-    public void setFlatNumber(String flatNumber) {
-        this.flatNumber = flatNumber;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("[")
-                .append(this.city).append(", ")
-                .append(this.zipCode).append(", ")
-                .append(this.street).append(", ")
-                .append(this.buildingNumber).append(", ")
-                .append(this.flatNumber).append("]");
-        return sb.toString();
     }
 }
--- src/main/java/PitzaNFryty/address/AddressReadDTO.java
+++ src/main/java/PitzaNFryty/address/AddressReadDTO.java
@@ -1,5 +1,12 @@
 package PitzaNFryty.address;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class AddressReadDTO {
 
     private Long id;
@@ -9,63 +16,4 @@ public class AddressReadDTO {
     private String buildingNumber;
     private String flatNumber;
 
-    public AddressReadDTO(Long id, String city, String zipCode, String street, String buildingNumber, String flatNumber) {
-        this.id = id;
-        this.city = city;
-        this.zipCode = zipCode;
-        this.street = street;
-        this.buildingNumber = buildingNumber;
-        this.flatNumber = flatNumber;
-    }
-
-    public AddressReadDTO() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getBuildingNumber() {
-        return buildingNumber;
-    }
-
-    public void setBuildingNumber(String buildingNumber) {
-        this.buildingNumber = buildingNumber;
-    }
-
-    public String getFlatNumber() {
-        return flatNumber;
-    }
-
-    public void setFlatNumber(String flatNumber) {
-        this.flatNumber = flatNumber;
-    }
 }
