88e59d5a03ca25bd3e8ad3885fae34b5674ed443
Michał Puła <michal.pula98@gmail.com>
2021-11-20 13:37:45 +0100

    refactor: add DrinkSize enum with its converter

--- src/main/java/PitzaNFryty/menu_item/drink/DrinkSize.java
+++ src/main/java/PitzaNFryty/menu_item/drink/DrinkSize.java
@@ -1,58 +1,24 @@
 package PitzaNFryty.menu_item.drink;
 
-import javax.persistence.*;
-import java.math.BigDecimal;
+public enum DrinkSize {
+    SMALL(0.3),
+    LARGE(0.7);
 
-@Entity
-@Table(name = "drinks_sizes")
-public class DrinkSize {
+    private final Double volume;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", unique = true, updatable = false)
-    private long id;
-
-    @Column(name = "size")
-    private String size;
-
-    @Column(name = "volume_in_liters")
-    private BigDecimal volume;
-
-    public DrinkSize(String size, BigDecimal volume) {
-        this.size = size;
+    DrinkSize(Double volume) {
         this.volume = volume;
     }
 
-    public DrinkSize() {
-
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
-    }
-
-    public BigDecimal getVolume() {
+    public Double getVolume() {
         return volume;
-    }
-
-    public void setVolume(BigDecimal volume) {
-        this.volume = volume;
     }
 
     @Override
     public String toString() {
-        return this.size + " - " + this.volume + "l";
+        final StringBuilder sb = new StringBuilder();
+        sb.append(super.toString().charAt(0));
+        sb.append(super.toString().substring(1).toLowerCase());
+        return sb.toString();
     }
 }
---
+++ src/main/java/PitzaNFryty/menu_item/drink/DrinkSizeConverter.java
@@ -0,0 +1,25 @@
+package PitzaNFryty.menu_item.drink;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class DrinkSizeConverter implements AttributeConverter<DrinkSize, String> {
+    @Override
+    public String convertToDatabaseColumn(DrinkSize drinkSize) {
+        return switch (drinkSize) {
+            case SMALL -> "Small";
+            case LARGE -> "Large";
+            default -> throw new IllegalArgumentException("Unknown " + drinkSize);
+        };
+    }
+
+    @Override
+    public DrinkSize convertToEntityAttribute(String string) {
+        return switch (string) {
+            case "Small" -> DrinkSize.SMALL;
+            case "Large" -> DrinkSize.LARGE;
+            default -> throw new IllegalArgumentException("Unknown " + string);
+        };
+    }
+}
