fb02bdda08f26e208030dde7737e955a391bd1a2
Michał Puła <michal.pula98@gmail.com>
2021-11-20 17:48:14 +0100

    refactor: change Order class to use Lombok and delete awful @LazyToOne

--- src/main/java/PitzaNFryty/order/Order.java
+++ src/main/java/PitzaNFryty/order/Order.java
@@ -3,17 +3,18 @@ package PitzaNFryty.order;
 import PitzaNFryty.address.Address;
 import PitzaNFryty.customer.Customer;
 import PitzaNFryty.menu_item.MenuItem;
-import PitzaNFryty.payment.Payment;
 import com.sun.istack.NotNull;
-import org.hibernate.annotations.LazyCollection;
-import org.hibernate.annotations.LazyCollectionOption;
-import org.hibernate.annotations.LazyToOne;
-import org.hibernate.annotations.LazyToOneOption;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "orders")
 public class Order {
@@ -23,17 +24,14 @@ public class Order {
     @Column(name = "id", unique = true, updatable = false)
     private Long id;
 
-    @ManyToOne(targetEntity = Customer.class, cascade = CascadeType.MERGE)
-    @LazyToOne(LazyToOneOption.NO_PROXY)
+    @ManyToOne(targetEntity = Customer.class)
     @JoinColumn(name = "customer_id")
     private Customer customer;
 
-    @ManyToOne(targetEntity = Address.class, cascade = CascadeType.MERGE)
-    @LazyToOne(LazyToOneOption.NO_PROXY)
+    @ManyToOne(targetEntity = Address.class)
     @JoinColumn(name = "address_id")
     private Address address;
 
-    @LazyCollection(LazyCollectionOption.FALSE)
     @ManyToMany(targetEntity = MenuItem.class, cascade = CascadeType.MERGE)
     @JoinTable(name = "orders_menu_items",
             joinColumns = {@JoinColumn(name = "order_id")},
@@ -41,93 +39,23 @@ public class Order {
     private List<? extends MenuItem> menuItems;
 
     @NotNull
-    @OneToOne(targetEntity = Payment.class, cascade = CascadeType.PERSIST)
-    @LazyToOne(LazyToOneOption.NO_PROXY)
-    @JoinColumn(name = "payment_id")
-    private Payment payment;
-
-    @NotNull
     @Column(name = "creation_time")
     private LocalDateTime creationTime;
 
-    public Order(Customer customer, Address address, List<MenuItem> menuItems, Payment payment, LocalDateTime creationTime) {
+    public Order(Customer customer, Address address, List<MenuItem> menuItems, LocalDateTime creationTime) {
         this.customer = customer;
         this.address = address;
         this.menuItems = menuItems;
-        this.payment = payment;
-        this.creationTime = creationTime;
-    }
-
-    public Order() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
-    public void setCustomer(Customer customer, boolean add) {
-        this.customer = customer;
-        if(customer != null && add) {
-            customer.addOrder(this, false);
-        }
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public List<? extends MenuItem> getMenuItems() {
-        return menuItems;
-    }
-
-    public void setMenuItems(List<? extends MenuItem> menuItems) {
-        this.menuItems = menuItems;
-    }
-
-    public Payment getPayment() {
-        return payment;
-    }
-
-    public void setPayment(Payment payment) {
-        this.payment = payment;
-    }
-
-    public LocalDateTime getCreationTime() {
-        return creationTime;
-    }
-
-    public void setCreationTime(LocalDateTime creationTime) {
         this.creationTime = creationTime;
     }
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("[")
-                .append(this.customer.getFirstName()).append(" ").append(this.customer.getLastName()).append(", ")
-                .append(this.address.getStreet()).append(this.address.getBuildingNumber()).append(", ");
-        sb.append("{");
-        menuItems.forEach(menuItem -> {
-            sb.append(menuItem.toString()).append(", ");
-        });
-        sb.append("}");
-        sb.append(this.payment.toString())
-        .append(this.creationTime).append("]");
+        final StringBuilder sb = new StringBuilder("Order{");
+        sb.append("id=").append(id);
+        sb.append(", menuItems=").append(menuItems);
+        sb.append(", creationTime=").append(creationTime);
+        sb.append('}');
         return sb.toString();
     }
 }
