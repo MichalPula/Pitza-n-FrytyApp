ccb4701f0e3e958237e4a8cbf0d74d90694740de
Michał Puła <michal.pula98@gmail.com>
2021-11-20 14:21:53 +0100

    refactor: change FriesReadDTO to use Lombok and store information in single map

--- src/main/java/PitzaNFryty/menu_item/fries/FriesReadDTO.java
+++ src/main/java/PitzaNFryty/menu_item/fries/FriesReadDTO.java
@@ -1,66 +1,18 @@
 package PitzaNFryty.menu_item.fries;
 
-import java.math.BigDecimal;
+import lombok.*;
+
+import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class FriesReadDTO {
 
     private String name;
-    private Map<Long, FriesSize> friesIdToSize;
-    private Map<Long, BigDecimal> friesIdToPrice;
+    private Map<Long, List<String>> friesIdToSizeWeightPriceList;
     private String imageURL;
-
-    public FriesReadDTO(String name, Map<Long, FriesSize> friesIdToSize, Map<Long, BigDecimal> friesIdToPrice, String imageURL) {
-        this.name = name;
-        this.friesIdToSize = friesIdToSize;
-        this.friesIdToPrice = friesIdToPrice;
-        this.imageURL = imageURL;
-    }
-
-    public FriesReadDTO() {
-    }
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Map<Long, FriesSize> getFriesIdToSize() {
-        return friesIdToSize;
-    }
-
-    public void setFriesIdToSize(Map<Long, FriesSize> friesIdToSize) {
-        this.friesIdToSize = friesIdToSize;
-    }
-
-    public Map<Long, BigDecimal> getDrinkIdToPrice() {
-        return friesIdToPrice;
-    }
-
-    public void setDrinkIdToPrice(Map<Long, BigDecimal> friesIdToPrice) {
-        this.friesIdToPrice = friesIdToPrice;
-    }
-
-    public String getImageURL() {
-        return imageURL;
-    }
-
-    public void setImageURL(String imageURL) {
-        this.imageURL = imageURL;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("FriesReadDTO{");
-        sb.append("name='").append(name).append('\'');
-        sb.append(", friesIdToSize=").append(friesIdToSize);
-        sb.append(", friesIdToPrice=").append(friesIdToPrice);
-        sb.append(", imageURL='").append(imageURL).append('\'');
-        sb.append('}');
-        return sb.toString();
-    }
 }
