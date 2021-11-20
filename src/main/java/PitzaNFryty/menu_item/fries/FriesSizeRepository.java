2ba0094a148128dad37c9893384a79e3f49a23bf
Michał Puła <michal.pula98@gmail.com>
2021-11-20 14:20:18 +0100

    refactor: remove FriesSizeRepository

--- src/main/java/PitzaNFryty/menu_item/fries/FriesSizeRepository.java
+++
@@ -1,8 +0,0 @@
-package PitzaNFryty.menu_item.fries;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FriesSizeRepository extends JpaRepository<FriesSize, Long> {
-}
