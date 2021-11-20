e3213b8a169d59bd50652db36358affe8618ae6e
Michał Puła <michal.pula98@gmail.com>
2021-11-20 17:34:26 +0100

    refactor: add PizzaSize enum with its converter

--- src/main/java/PitzaNFryty/menu_item/pizza/PizzaSize.java
+++ src/main/java/PitzaNFryty/menu_item/pizza/PizzaSize.java
@@ -1,59 +1,22 @@
 package PitzaNFryty.menu_item.pizza;
 
-import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
-@Entity
-@Table(name = "pizzas_sizes")
-public class PizzaSize {
+@Getter
+@AllArgsConstructor
+public enum PizzaSize {
+    SMALL(30),
+    MEDIUM(40),
+    LARGE(50);
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", unique = true, updatable = false)
-    private long id;
-
-    @Column(name = "size")
-    private String size;
-
-    @Column(name = "diameter_in_cm")
-    private Integer diameter;
-
-    public PizzaSize(String size, Integer diameter) {
-        this.size = size;
-        this.diameter = diameter;
-    }
-
-    public PizzaSize() {
-
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
-    }
-
-    public Integer getDiameter() {
-        return diameter;
-    }
-
-    public void setDiameter(Integer diameter) {
-        this.diameter = diameter;
-    }
+    private final Integer diameter;
 
     @Override
     public String toString() {
-
-        return this.size + " - " + this.diameter + "cm";
-
+        final StringBuilder sb = new StringBuilder();
+        sb.append(super.toString().charAt(0));
+        sb.append(super.toString().substring(1).toLowerCase());
+        return sb.toString();
     }
 }
