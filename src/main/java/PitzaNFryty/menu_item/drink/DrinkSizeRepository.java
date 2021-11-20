de105ea92d363b147084d368ffecf52db6eba722
Michał Puła <michal.pula98@gmail.com>
2021-11-20 13:41:50 +0100

    refactor: delete DrinkSizeRepository

--- src/main/java/PitzaNFryty/menu_item/drink/DrinkSizeRepository.java
+++
@@ -1,8 +0,0 @@
-package PitzaNFryty.menu_item.drink;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DrinkSizeRepository extends JpaRepository<DrinkSize, Long> {
-}
