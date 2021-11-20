67566448bc2752e1590a5bd5494976eec20c5466
Michał Puła <michal.pula98@gmail.com>
2021-11-20 17:35:40 +0100

    refactor: refactor getAll method to use new DTO with enum

--- src/main/java/PitzaNFryty/menu_item/pizza/PizzaServiceImpl.java
+++ src/main/java/PitzaNFryty/menu_item/pizza/PizzaServiceImpl.java
@@ -3,59 +3,60 @@ package PitzaNFryty.menu_item.pizza;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class PizzaServiceImpl implements PizzaService {
 
     private final PizzaRepository pizzaRepository;
-    private final PizzaSizeRepository pizzaSizeRepository;
 
     @Autowired
-    public PizzaServiceImpl(PizzaRepository pizzaRepository, PizzaSizeRepository pizzaSizeRepository) {
+    public PizzaServiceImpl(PizzaRepository pizzaRepository) {
         this.pizzaRepository = pizzaRepository;
-        this.pizzaSizeRepository = pizzaSizeRepository;
     }
 
     @Override
     public List<PizzaReadDTO> getAll() {
         List<Pizza> pizzas = pizzaRepository.findAll();
-        List<PizzaSize> pizzaSizes = pizzaSizeRepository.findAll();
-        int amountOfPizzas = pizzas.size();
-        int amountOfSamePizzas = pizzaSizes.size();
+        Map<String, List<Pizza>> pizzaNameToPizzasSizes = new LinkedHashMap<>();
+
+        pizzas.forEach(pizza -> {
+            String friesName = pizza.getName();
+            if(pizzaNameToPizzasSizes.get(friesName) == null){
+                pizzaNameToPizzasSizes.put(friesName, new ArrayList<>(List.of(pizza)));
+            } else {
+                pizzaNameToPizzasSizes.get(friesName).add(pizza);
+            }
+        });
+
 
         List<PizzaReadDTO> pizzaReadDTOList = new ArrayList<>();
-
-        for(int i = 0; i <= amountOfPizzas; i += amountOfSamePizzas) {
-            if(i > amountOfPizzas - amountOfSamePizzas) {
-                break;
-            }
-            List<Pizza> samePizzas = pizzas.subList(i, i + amountOfSamePizzas);
+        pizzaNameToPizzasSizes.forEach((pizzaName, pizzaInAllSizes) -> {
             PizzaReadDTO pizzaReadDTO = new PizzaReadDTO();
-            Map<Long, PizzaSize> pizzaIdToSize = new LinkedHashMap<>();
-            Map<Long, BigDecimal> pizzaIdToPrice = new LinkedHashMap<>();
-            Map<Long, String> ingredients = new LinkedHashMap<>();
-            Map<Long, String> sauces = new LinkedHashMap<>();
+            pizzaReadDTO.setName(pizzaName);
 
-            samePizzas.forEach(pizza -> {
-                pizzaIdToSize.put(pizza.getId(), pizza.getPizzaSize());
-                pizzaIdToPrice.put(pizza.getId(), pizza.getPrice());
+            Map<Long, List<String>> pizzaIdToSizeDiameterPriceList = new LinkedHashMap<>();
+            pizzaInAllSizes.forEach(pizza -> {
+                String pizzaSize = pizza.getPizzaSize().toString();
+                String pizzaDiameter = pizza.getPizzaSize().getDiameter().toString() + "cm";
+                String pizzaPrice = pizza.getPrice().toString();
+                pizzaIdToSizeDiameterPriceList.put(pizza.getId(), new ArrayList<>(Arrays.asList(pizzaSize, pizzaDiameter, pizzaPrice)));
             });
-            pizzaReadDTO.setName(samePizzas.get(0).getName());
-            pizzaReadDTO.setPizzaIdToSize(pizzaIdToSize);
-            pizzaReadDTO.setPizzaIdToPrice(pizzaIdToPrice);
-            samePizzas.get(0).getIngredients().forEach(ingredient -> ingredients.put(ingredient.getId(), ingredient.getName()));
-            pizzaReadDTO.setIngredients(ingredients);
-            samePizzas.get(0).getSauces().forEach(sauce -> sauces.put(sauce.getId(), sauce.getName()));
-            pizzaReadDTO.setSauces(sauces);
-            pizzaReadDTO.setImageURL(samePizzas.get(0).getImageURL());
+            pizzaReadDTO.setPizzaIdToSizeDiameterPriceList(pizzaIdToSizeDiameterPriceList);
+
+            Map<Long, String> ingredientIdToName= new LinkedHashMap<>();
+            pizzaInAllSizes.get(0).getIngredients().forEach(ingredient -> ingredientIdToName.put(ingredient.getId(), ingredient.getName()));
+            pizzaReadDTO.setIngredients(ingredientIdToName);
+
+            Map<Long, String> sauceIdToName = new LinkedHashMap<>();
+            pizzaInAllSizes.get(0).getSauces().forEach(sauce -> sauceIdToName.put(sauce.getId(), sauce.getName()));
+            pizzaReadDTO.setSauces(sauceIdToName);
+
+            pizzaReadDTO.setImageURL(pizzaInAllSizes.get(0).getImageURL());
 
             pizzaReadDTOList.add(pizzaReadDTO);
-        }
+        });
+
         return pizzaReadDTOList;
     }
 }
