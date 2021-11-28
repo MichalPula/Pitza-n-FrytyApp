64018f59153f5c2dc9c988fde69acb76fc6aebca
Michał Puła <michal.pula98@gmail.com>
2021-11-28 23:30:01 +0100

    feat: adjust configure method in SecurityConfig by adding endpoints

--- src/main/java/PitzaNFryty/SpringSecurityConfig.java
+++ src/main/java/PitzaNFryty/SpringSecurityConfig.java
@@ -1,6 +1,9 @@
 package PitzaNFryty;
 
+import PitzaNFryty.customer.RegisteredUser.RegisteredUserRepository;
 import PitzaNFryty.jwt_authentication.UserDetailsServiceImpl;
+import PitzaNFryty.jwt_authentication.filters.JwtAuthenticationFilter;
+import PitzaNFryty.jwt_authentication.filters.JwtAuthorizationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +12,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
@@ -17,10 +21,13 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsServiceImpl userDetailServiceImpl;
 
+    private final RegisteredUserRepository registeredUserRepository;
+
 
     @Autowired
-    public SpringSecurityConfig(UserDetailsServiceImpl userDetailServiceImpl) {
+    public SpringSecurityConfig(UserDetailsServiceImpl userDetailServiceImpl, RegisteredUserRepository registeredUserRepository) {
         this.userDetailServiceImpl = userDetailServiceImpl;
+        this.registeredUserRepository = registeredUserRepository;
     }
 
     @Bean
@@ -44,6 +51,17 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new JwtAuthenticationFilter(authenticationManager(), "/login"))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(), this.registeredUserRepository))
+                .authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/everyone").permitAll()
+                .antMatchers("/authenticated").authenticated()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/manager").hasAnyRole("MANAGER", "ADMIN");
     }
 }
